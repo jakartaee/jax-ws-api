bd4204de29928d38f924b6c5fe615f590531f2b5
Lukas Jungmann <lukas.jungmann@oracle.com>
2018-11-09 12:19:12 +0100

    Fixes #46: artifactId should be javax.xml.ws-api
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- api/test/setup.sh
+++ api/test/setup.sh
@@ -13,7 +13,7 @@ rm -rf endorsed
 mkdir endorsed
 cd ..
 mvn clean package
-cp target/jaxws-api*.jar test/endorsed
+cp target/javax.xml.ws-api*.jar test/endorsed
 
 echo "endorsed dir: "`pwd`/test/endorsed
 ls -al test/endorsed
