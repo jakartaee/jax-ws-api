1ec117f2bee8b483ed25f22da7f0f24f1e6264e8
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-07-30 11:25:44 +0200

    fix references to Java EE in javadoc
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- api/src/main/java/javax/xml/ws/WebServiceRef.java
+++ api/src/main/java/javax/xml/ws/WebServiceRef.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -31,7 +31,7 @@ import javax.annotation.Resource;
  * support multiple threads.
  *
  * <p>
- * Web service references are resources in the Java EE 5 sense.
+ * Web service references are resources in the Jakarta EE sense.
  * The annotations (for example, {@link Addressing}) annotated with
  * meta-annotation {@link WebServiceFeatureAnnotation}
  * can be used in conjunction with {@code WebServiceRef}.
