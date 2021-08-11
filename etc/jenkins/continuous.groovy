026b78629682940db496c7c6342b9e8bdad31ca9
Maxim Nesen <maxim.nesen@oracle.com>
2021-08-11 15:20:04 +0200

    post-build tools update
    Signed-off-by: Maxim Nesen <maxim.nesen@oracle.com>

--- etc/jenkins/continuous.groovy
+++ etc/jenkins/continuous.groovy
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+// Copyright (c) 2019, 2021 Oracle and/or its affiliates. All rights reserved.
 //
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -64,7 +64,7 @@ pipeline {
                 sshagent([SSH_CREDENTIALS_ID]) {
                     sh 'etc/jenkins/continuous.sh'
                 }
-                recordIssues(tools: [spotBugs(useRankAsPriority: true)])
+		recordIssues(tools: [java(), javaDoc(), spotBugs(useRankAsPriority: true)])
             }
         }
         
